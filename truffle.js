284cf9767bbdb61ce29a0766fbd330b941e2d4ec
Fabio P <fp555@users.noreply.github.com>
2017-07-25 22:54:09 +0200

    truffle.js compatibile per entrambi + nuovi script personalizzati npm

--- truffle.js
+++ truffle.js
@@ -1,9 +1,14 @@
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 22000,
-      network_id: "*" // Match any network id
+    networks: {
+        quorum: {
+            host: "localhost",
+            port: 22000,
+            network_id: "*" // Match any network id
+        },
+        testrpc: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*" // Match any network id
+        }
     }
-  }
 };
