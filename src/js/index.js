e2f1d57eb57ddc17dfb9391fbcf49406e9be059d
Fabio P <fp555@users.noreply.github.com>
2017-07-26 20:03:45 +0200

    controllo del role in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,38 @@
 App = {
-    contracts: {}, // variabile contratto
+    contract: {}, // variabile contratto
+    web3Provider: null,
     
     init: function() {
         // inizializza web3
         if (typeof web3 !== 'undefined') { // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-            window.web3 = new Web3(web3.currentProvider); // Use Mist/MetaMask's provider
+            // Use Mist/MetaMask's provider
+            App.web3Provider = web3.currentProvider;
+            web3 = new Web3(web3.currentProvider);
         } else {
             console.log('No web3? You should consider trying MetaMask!')
             // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-            window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+            web3 = new Web3(App.web3Provider);
         }
-        
         // inizializza il contratto
         $.getJSON('Prescriptions.json', function(data) {
-            var prescriptionsArtifact = data;
-            App.contracts.Prescriptions = TruffleContract(prescriptionsArtifact);
-            App.contracts.Prescriptions.setProvider(window.web3);
-            
+            App.contract.Prescriptions = TruffleContract(data);
+            App.contract.Prescriptions.setProvider(App.web3Provider);
+        }).then(function() {
+            return App.checkRole();
         });
-        
-        return App.chekRole();
-        // return *bindevents per i clic*
-    }
-
+    },
+    
     checkRole: function() {
-
-        var account;
-        web3.eth.getAccounts(function(err,accounts) {
-            if(err) {
-                console.log(err); 
-            } else {
-                account = accounts[0]; 
-            }
-
+        App.contract.Prescriptions.deployed().then(function(instance) {
+            return instance.getRole.call();
+        }).then(function(role) {
+            console.log(role);
         });
-        App.contracts.Prescriptions.deployed().then(function(instance) {
-            return instance.getRole.call(account);
-        }).then(function(result){
-            console.log(result);
-        });
+    },
+    
+    registra: function() {
+        
     }
 };
 
