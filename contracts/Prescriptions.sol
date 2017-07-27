4bda6facfbe26410627bf1edc9eb519ffbc999a0
Fabio P <fp555@users.noreply.github.com>
2017-07-27 15:38:02 +0200

    redirect ajax in base al role funzionante

--- contracts/Prescriptions.sol
+++ contracts/Prescriptions.sol
@@ -5,29 +5,27 @@ contract Prescriptions {
 	struct InfoMedico {
 		string name;
 		string surname;
-		string specialization;
 	}
 
 	//mapping utente-ruoli
 	mapping (address => string) internal roles ;
 
-	//mapping medico-anagrafica
+	//mapping accounts-anagrafica
 	mapping (address => InfoMedico) internal medici;
 	
 	function getRole() public returns (string) {
 		return roles[msg.sender];
 	}
 
-	function setMedico(string nome, string cognome, string specializzazione, string role) public {
+	function setMedico(string nome, string cognome, string role) public {
 		medici[msg.sender] = InfoMedico({
 			name : nome,
-			surname : cognome,
-			specialization : specializzazione
+			surname : cognome
 			});
 		roles[msg.sender] = role;
 	}
 
-	function getMedico() public returns (string, string, string) {
-		return (medici[msg.sender].name, medici[msg.sender].surname, medici[msg.sender].specialization);
+	function getMedico() public returns (string, string) {
+		return (medici[msg.sender].name, medici[msg.sender].surname);
 	}
 }
--- src/js/index.js
+++ src/js/index.js
@@ -13,34 +13,40 @@ App = {
         }
         // inizializza il contratto
         $.getJSON('Prescriptions.json', function(data) {
-            App.contract.Prescriptions = TruffleContract(data);
-            App.contract.Prescriptions.setProvider(web3.currentProvider);
+            App.contract = TruffleContract(data);
+            App.contract.setProvider(web3.currentProvider);
         }).then(function() {
             return App.checkRole();
         });
     },
     
     checkRole: function() {
-        App.contract.Prescriptions.deployed().then(function(instance) {
+        App.contract.deployed().then(function(instance) {
             return instance.getRole.call();
         }).then(function(role) {
             console.log(role);
+            if(role === "medic" || role === "farma") {
+                $(".container").load('pages/' + role + '.html', function() {
+                    //execute here after load completed
+                });
+            }
         });
     },
     
     registra: function(form) {
-        web3.eth.getAccounts(function(error,accounts){
+        web3.eth.getAccounts(function(error,accounts) {
             if(error) {
                 console.log(error);
             }
             var account = accounts[0];
-            App.contract.Prescriptions.deployed().then(function(instance) {
-                return instance.setMedico(form.nome, form.cognome, form.specializzazione, form.ruolo, {from: account});
+            App.contract.deployed().then(function(instance) {
+                return instance.setMedico(form.nome, form.cognome, form.ruolo, {from: account});
             }).then(function() {
-                App.contract.Prescriptions.deployed().then(function(instance) {
+                App.contract.deployed().then(function(instance) {
                     return instance.getMedico.call();
                 }).then(function(info){
                     console.log(info);
+                    return App.checkRole();
                 });
             });
         });
